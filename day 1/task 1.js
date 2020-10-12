5f05446c03f85646343c06d07355c1e4ac22b8b5
yulia tsygankova <yulia.tsygankova@constantcontact.com>
2020-10-12 12:05:04 -0400

    cleanup

--- day 1/task 1.js
+++ day 1/task 1.js
@@ -2,15 +2,14 @@ const username = 'YT';
 
 function usersGreeting() {
     let time = new Date().getHours();
-    console.log(time);
     switch (true) {
-        case 0 <= time < 5:
+        case 0 <= time && time < 5:
             return console.log('Good night, ' + username);
-        case 5 <= time < 10:
+        case 5 <= time && time < 10:
             return console.log('Good morning, ' + username);
-        case 10 <= time < 17:
+        case 10 <= time && time < 17:
             return console.log('Good afternoon, ' + username);
-        case 17 <= time < 0:
+        case 17 <= time && time < 0:
             return console.log('Good evening, ' + username);
     }
 }
