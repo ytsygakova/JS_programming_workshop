a67d39a7b84946eafe871190b89339cd87a9f373
yulia tsygankova <yulia.tsygankova@constantcontact.com>
2020-10-14 21:13:24 -0400

    added solution for day 3 task 2

---
+++ day 3/task 2.js
@@ -0,0 +1,29 @@
+function calculateWordFrequency(text, repeatsMax) {
+    const punctuation = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    const newDataArray = text.toLowerCase()
+        .replace(punctuation, ',')
+        .split(/[\s,]+/)
+     // .reduce((map, word) =>   This looks cool but I don't understand how it works
+     //         Object.assign(map, {
+     //             [word]: (map[word]) ? map[word] + 1 : 1,
+     //         }),
+     //     {}
+     // );
+
+    const freqMap = {};
+    newDataArray.forEach(function(w) {
+        if (!freqMap[w]) {
+            freqMap[w] = 0;
+        }
+        freqMap[w] += 1;
+    });
+
+    for (const number in freqMap) {
+        (freqMap[number] <= repeatsMax || number === '') && delete freqMap[number];
+        console.log(`${number}: ${freqMap[number]}`);
+    }
+
+    return freqMap;
+}
+
+console.log(calculateWordFrequency(' And we are here... Here - and We and Are: The end.', 2));
