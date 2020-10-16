084fb72ec929d947e76d9c16bbcc5dee7d82fca6
yulia tsygankova <yulia.tsygankova@constantcontact.com>
2020-10-15 21:06:59 -0400

    cleanup

--- day 4/task 1.js
+++ day 4/task 1.js
@@ -1,11 +1,12 @@
 function arrayContainsSum(array, sum) {
-   for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < (array.length); j++){
-            if ((array[i] !== array[j])) {
-                return array[i] + array[j] === sum;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < (array.length); j++) {
+            if ((array[i] !== array[j]) && (array[i] + array[j] === sum)) {
+                return  true;
             }
         }
     }
+    return false;
 }
 
-console.log(arrayContainsSum([1, 2, 3, 4, 152], 33333));
+console.log(arrayContainsSum([1, 2, 3, 4, 152], 887));
