cfdcef691e7ac528f437ffbee800078844c8aa08
yulia tsygankova <yulia.tsygankova@constantcontact.com>
2020-10-19 10:40:59 -0400

    added solution for day 4 task 2 with some()

--- day 4/task 2.js
+++ day 4/task 2.js
@@ -1,27 +1,11 @@
 function arrayContainsSumUpgrade(array, sum) {
-
-    let numObject = array.reduce((accumulator, currentValue) => {
-        accumulator[currentValue] = currentValue;
-        return accumulator;
-    }, {});
-
-    // Same thing with a loop
-    //
-    // let numObject = {};
-    //
-    // for (let i = 0; i < array.length; i++) {
-    //     let thisNum = array[i];
-    //     numObject[thisNum] = i;
-    // }
-
     for (let i = 0; i < array.length; i++) {
-        let diff = sum - array[i];
-        if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
-            return true;
+        if (array.some(currentValue => currentValue === sum - array[i])){
+            return true
         }
     }
     return false;
 }
 
-console.log(arrayContainsSumUpgrade([1, 2, 3, 4, 152], 9999));
+console.log(arrayContainsSumUpgrade([1, 2, 3, 4, 152], 155));
 
